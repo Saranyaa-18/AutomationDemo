cef87a7858ead5650c211539e7653ebd1b671e54
Saranyaa-18 <Raju.saranyaa18@gmail.com>
2025-03-21 06:39:38 +0530

    Commit message for Demo

---
+++ src/git/Animal.java
@@ -0,0 +1,20 @@
+package git;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Class Animal");
+	}
+	
+	public void cat()
+	{
+		System.out.println("Cat");
+	}
+
+	public void dog()
+	{
+		System.out.println("Dog");
+	}
+}
---
+++ src/git/Flowers.java
@@ -0,0 +1,21 @@
+package git;
+
+public class Flowers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Class Flowers");
+	}
+	
+	public void cat()
+	{
+		System.out.println("Rose");
+	}
+
+	public void dog()
+	{
+		System.out.println("Jasmine");
+	}
+	
+}
